40a87e3b7a6fc643220d2920f70e5f76437d1bed
Krishna Suryavanshi <krishna.suryavanshiac@gmail.com>
2024-07-24 01:21:33 +0530

    Done code Formatting

--- lib/screens/articel_view.dart
+++ lib/screens/articel_view.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:myapp/api/medium_api.dart';
 import 'package:myapp/modals/not_valid_data.dart';
 import 'package:myapp/modals/response_modal.dart';
--- lib/screens/parse_webview.dart
+++ lib/screens/parse_webview.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
